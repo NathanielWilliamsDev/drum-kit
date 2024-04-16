eced7e4cc2f3489c18c69965fc05ad32b25a4ade
Nathaniel <nathaniel.c.williams@hotmail.com>
2024-04-16 23:56:09 +1000

    Slight styling tweak, added comments to JS file for potential bug fix

--- script.js
+++ script.js
@@ -10,7 +10,7 @@ window.addEventListener("keydown", function(event){
     
     key.classList.add("pressed"); // Give the pressed key the class "pressed"
 
-    function removeTransition(event){
+    function removeTransition(event){ // removes the "pressed" class after sound is executed
         if(event.propertyName !== "transform") return; // Skip if not a trasform (longest one?)
         this.classList.remove("pressed");
     }
@@ -18,3 +18,27 @@ window.addEventListener("keydown", function(event){
     let keys = document.querySelectorAll(".key"); // Array (Node List) of all elements of class .key
     keys.forEach(key => key.addEventListener("transitionend", removeTransition));
 });
+
+/* 
+The transition sticks when a key is pressed for too long (2-3 seconds),
+    Below is a fix for this.
+
+function handleKeyEvent(e){
+    let audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
+    let key = document.querySelector(`.key[data-key="${e.keyCode}"]`); // assigning the key to a variable
+    if(!audio) return; // Pressed a key that has no audio tag, just return
+    audio.currentTime = 0; // Lets spamming, resets the audio time when another keydown occurs
+    audio.play();
+    
+    if (e.type === 'keydown') {
+        key.classList.add("pressed");
+    } else if (e.type === 'keyup') {
+        key.classList.remove("pressed");
+    }
+
+    
+}
+
+window.addEventListener("keydown", handleKeyEvent);
+window.addEventListener("keyup", handleKeyEvent);
+*/
